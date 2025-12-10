c536b7e73f50bfa3eb7857d3d6924bca1cd4f26c
Maria Mileeva <mileeva.mary@gmail.com>
2025-12-10 16:38:28 +0400

    save messages from the bot to the db

--- app/main.py
+++ app/main.py
@@ -4,34 +4,42 @@ from contextlib import asynccontextmanager
 import aiosqlite
 import requests
 from fastapi import FastAPI, status
-from pydantic_models import Update
-from sql_scripts import SQL_SCRIPT_STARTUP
+from pydantic_models import Env, Update
+from sql_scripts import (
+    SQL_CREATE_MESSAGE,
+    SQL_CREATE_TABLE_CATEGORIES,
+    SQL_CREATE_TABLE_EXPENSES,
+    SQL_CREATE_TABLE_MESSAGES,
+    SQL_CREATE_TABLE_UPDATES,
+    SQL_CREATE_TABLE_USERS,
+    SQL_CREATE_UPDATE,
+    SQL_CREATE_USER_IF_NOT_EXISTS,
+)
 
 
-async def create_db_and_tables() -> None:
+async def create_db_and_tables(env: Env) -> None:
     """Init & connect to database"""
 
-    sqlite_file_name = "database.db"
-    async with aiosqlite.connect(sqlite_file_name) as db:
-        await db.executescript(SQL_SCRIPT_STARTUP)
+    async with aiosqlite.connect(env.db_file_name) as db:
+        await db.executescript(SQL_CREATE_TABLE_USERS)
+        await db.executescript(SQL_CREATE_TABLE_MESSAGES)
+        await db.execute(SQL_CREATE_TABLE_UPDATES)
+        await db.executescript(SQL_CREATE_TABLE_CATEGORIES)
+        await db.execute(SQL_CREATE_TABLE_EXPENSES)
+        await db.commit()
 
-async def set_telegram_webhook() -> None:
+
+async def set_telegram_webhook(env: Env) -> None:
     """Set WebHook for Telegram Bot"""
 
-    bot_token = os.getenv('TG_BOT_TOKEN')
-    webhook_api_route = f'https://api.telegram.org/bot{bot_token}/'
-    webhook_api_method = 'setWebhook'
-    request_url = f'{webhook_api_route}{webhook_api_method}'
-    public_url = os.getenv('NGROK_URL')
-    if not public_url:
-        raise ValueError('public_url is not set')
+    webhook_api_route = f'https://api.telegram.org/bot{env.tg_bot_token}/setWebhook'
     message_api_method = '/message/'
-    message_api_route = public_url + message_api_method
+    message_api_route = env.ngrok_url + message_api_method
     body = {
         'url': message_api_route,
         'drop_pending_updates': False,
     }
-    response = requests.post(request_url, data=body)
+    response = requests.post(webhook_api_route, data=body)
     if response.status_code != status.HTTP_200_OK:
         raise Exception
 
@@ -39,9 +47,9 @@ async def set_telegram_webhook() -> None:
 @asynccontextmanager
 async def lifespan(app: FastAPI):  # noqa: ARG001
     """Check and create db and set webhook before start an app"""
-
-    await create_db_and_tables()
-    await set_telegram_webhook()
+    env = Env.from_env(os.getenv)
+    await create_db_and_tables(env=env)
+    await set_telegram_webhook(env=env)
     yield
 
 
@@ -55,4 +63,11 @@ async def get() -> dict:
 
 @app.post('/message/')
 async def post(update: Update) -> Update:
+    env = Env.from_env(os.getenv)
+    async with aiosqlite.connect(env.db_file_name) as db:
+        await db.execute(
+            SQL_CREATE_USER_IF_NOT_EXISTS.format(user=update.message.user))
+        await db.execute(SQL_CREATE_MESSAGE.format(message=update.message))
+        await db.execute(SQL_CREATE_UPDATE.format(update=update))
+        await db.commit()
     return update
